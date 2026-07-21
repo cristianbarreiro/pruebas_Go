29d1b4dad0ab27c2c4a6823883e19846af1ec39b
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2026-07-20 23:17:06 -0300

    fix: formatos ejemplos

--- format1.go
+++ format1.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	i := 42
-	fmt.Println("Here is Println with defaults: i = ", i)
+
+	fmt.Println("Here is Println with defaults: i =", i)
 	fmt.Printf("Here is Printf with format: i = %d \n", i)
 	fmt.Printf("Here is Printf with width 10d format: i = %10d \n", i)
 	fmt.Printf("Here is Printf with binary format: i = %b \n", i)
@@ -12,4 +13,8 @@ func main() {
 	fmt.Printf("Here is Printf with x hexadecimal formats: i = %x \n", i)
 	fmt.Printf("Here is Printf with O octal formats: i = %O \n", i)
 
+	x := 1000 * i
+
+	fmt.Printf("Here is Printf with g format: i = %g \n", float64(x))
+	fmt.Printf("Here is Printf with 10.2g format: i = %10.2g \n", float64(x))
 }
