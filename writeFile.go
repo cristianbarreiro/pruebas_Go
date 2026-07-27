451a1bcc4eb552b3f2f2cab29f34dc2c11ffeed9
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2026-07-27 14:10:58 -0300

    feat: writeFile, FileLecture & subSlicing

---
+++ writeFile.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func checkFile(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	fmt.Println("Write File of Int Program locally to intData")
+	file, err := os.Create("intData")
+	checkFile(err)
+	defer file.Close()
+	var dataSlice = []int{45, 66, 88, 99, 10}
+	for _, v := range dataSlice {
+		_, err := fmt.Fprintf(file, "%d\n", v)
+
+		checkFile(err)
+	}
+
+	fmt.Println(dataSlice)
+}
